4b506e9369194e5bf9770e2d25a9535470e52090
Alan Watson <alphawhiskey82@yahoo.com>
2022-08-16 07:52:34 -0400

    test zarr creation

---
+++ stack_to_multiscale_ngff/bil_test/test_zarr.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 15 20:57:49 2022
+
+@author: alpha
+"""
+
+import zarr
+import numpy as np
+import sys
+
+path = r'C:\code\stack_to_multiscale_ngff\stack_to_multiscale_ngff'
+if path not in sys.path:
+    sys.path.append(path)
+    
+from h5_shard_store import H5_Shard_Store as h5s
+
+
+root = 'c:/code/testZarr'
+
