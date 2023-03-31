4661b408ff147dbea30d6675f8600fd997359f85
Alan M Watson <alan.watson@pitt.edu>
2023-03-31 17:11:21 -0400

    Support for reshaping of dataset to support stacks that are not along the z axis

--- stack_to_multiscale_ngff/utils.py
+++ stack_to_multiscale_ngff/utils.py
@@ -23,7 +23,38 @@ def get_size_GB(shape,dtype):
     
     return current_size
     
+def axis_reorganizer(array=None,input_orientation='tczyx'):
+    '''
+    Return a tuple which transposes an array to 'tczyx' orientation given the known orientation
+    Currently assumes that dims 0,1 are always tc
 
+    input_orientation = string designating 3-axis 'x','y','z' orientation
+    Examples: 'tczyx','tcxyz','tcyzx','tczxy'
+    '''
+
+    assert isinstance(input_orientation,str)
+    assert len(input_orientation) == 5
+    assert 't' in input_orientation and input_orientation[0] == 't'
+    assert 'c' in input_orientation and input_orientation[1] == 'c'
+    assert 'z' in input_orientation
+    assert 'y' in input_orientation
+    assert 'x' in input_orientation
+
+    if array is not None:
+        assert len(array.shape) == 5
+
+    orientations = {
+        'tczyx': (0,1,2,3,4),
+        'tczxy': (0,1,2,4,3),
+        'tcxzy': (0,1,3,4,2),
+        'tcxyz': (0,1,4,3,2),
+        'tcyzx': (0,1,3,2,4),
+        'tcyxz': (0,1,4,2,3)
+    }
+    if array is None:
+        return orientations[input_orientation]
+    if array is not None:
+        return array.transpose(orientations[input_orientation])
 
 def optimize_chunk_shape_3d(image_shape,origional_chunks,dtype,chunk_limit_GB):
     
